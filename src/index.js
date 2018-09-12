138f052245af9562f1a18794918062e8b4a5f61c
Anna Kolesnikova <ani.kolesnikova@gmail.com>
2018-09-12 11:53:02 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,42 +1,40 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
+  obj = {};
 
-    obj = {};
-
-    if (currency > 10000) {
-        obj.error = "You are rich, my friend! We don't have so much coins for exchange";
-        return obj;
-    }
-    else if (currency <= 0) {
-        return obj;
-    }
-
-    let H = Math.trunc(currency / 50);
-    let Q = Math.trunc((currency - H * 50) / 25);
-    let D = Math.trunc((currency - H * 50 - Q * 25) / 10);
-    let N = Math.trunc((currency - H * 50 - Q * 25 - D * 10) / 5);
-    let P = currency - H * 50 - Q * 25 - D * 10 - N * 5;
-
-    if (H != 0) {
-        obj.H = H;
-    }
-
-    if (Q != 0) {
-        obj.Q = Q;
-    }
-
-    if (D != 0) {
-        obj.D = D;
-    }
-
-    if (N != 0) {
-        obj.N = N;
-    }
-
-    if (P != 0) {
-        obj.P = P;
-    }
-
+  if (currency > 10000) {
+    obj.error =
+      "You are rich, my friend! We don't have so much coins for exchange";
     return obj;
+  } else if (currency <= 0) {
+    return obj;
+  }
 
-}
+  let H = Math.trunc(currency / 50);
+  let Q = Math.trunc((currency - H * 50) / 25);
+  let D = Math.trunc((currency - H * 50 - Q * 25) / 10);
+  let N = Math.trunc((currency - H * 50 - Q * 25 - D * 10) / 5);
+  let P = currency - H * 50 - Q * 25 - D * 10 - N * 5;
+
+  if (H != 0) {
+    obj.H = H;
+  }
+
+  if (Q != 0) {
+    obj.Q = Q;
+  }
+
+  if (D != 0) {
+    obj.D = D;
+  }
+
+  if (N != 0) {
+    obj.N = N;
+  }
+
+  if (P != 0) {
+    obj.P = P;
+  }
+
+  return obj;
+};
